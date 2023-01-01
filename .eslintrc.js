2bd3ea2d99b7047e615f34a8c722468124f801c7
Shafa Alafghany <alafghanyshafa@gmail.com>
2023-01-01 18:52:53 +0800

    initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+        "linebreak-style": "off",
+        "no-underscore-dangle": "off",
+        "class-methods-use-this": "off",
+        "no-unused-vars": "off",
+        "no-console": "off"
+    }
+};
