4a8c41b30a75bda2e94aef2cd54eab2a3c723403
Shafa Alafghany <alafghanyshafa@gmail.com>
2023-01-01 19:17:20 +0800

    add: custom response

---
+++ src/Commons/constants/response.js
@@ -0,0 +1,33 @@
+module.exports = {
+  ERROR: (res, code, status, message) => {
+    const response = res.response({
+      status,
+      message,
+    })
+    response.code(code)
+    return response
+  },
+  SUCCESS: (res, code, status, message, data,) => {
+    let result
+    if (message === '' || message == null) {
+      result = {
+        status,
+        data,
+      }
+    } else if (data === '' || data == null) {
+      result = {
+        status,
+        message,
+      }
+    } else {
+      result = {
+        status,
+        message,
+        data,
+      }
+    }
+    const response = res.response(result)
+    response.code(code)
+    return response
+  },
+}
